64aa980cfe55acd38fdbb7e1e774a9a2472b7b06
Thales Janisch Santos <33602977+Thales3006@users.noreply.github.com>
2023-11-22 11:47:19 -0300

    Update bibs.h

--- bibs.h
+++ bibs.h
@@ -21,15 +21,17 @@ typedef struct pista{
 }pista;
 
 void criar_pista(pista** a);
-void add_to_pista(pista** a,int pos,int codigo, char modelo[], char destino[], int distancia, int tempo_de_voo);
-void del_pista(pista** a,int pos);
-void rem_from_pista(pista** a,int pos);
+void add_to_pista(pista** a, int pos, int codigo, char modelo[], char destino[], int distancia, int tempo_de_voo, int velocidade, int estado);
+void del_pista(pista** a, int pos);
+void rem_from_pista(pista** a, int pos);
 void imprime_pista(pista* a);
-void add_aviao(aviao** a, int codigo, char modelo[], char destino[], int distancia, int tempo_de_voo);
-void del_aviao(aviao** a,int pos);
+void add_aviao(aviao** a, int codigo, char modelo[], char destino[], int distancia, int tempo_de_voo,int velocidade, int estado);
+void del_aviao(aviao** a, int pos);
 void del_all_aviao(aviao** a);
 void del_all_pista(pista** a);
 void imprime_aviao(aviao* a);
 int tamanho_aviao(aviao* a);
 int tamanho_pista(pista* a);
 int tamanho_in_pista(pista* a);
+aviao* info_Pista(pista* a, int posP, int posA);
+aviao* info(aviao* a, int pos);
