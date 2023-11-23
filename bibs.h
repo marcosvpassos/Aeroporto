e9c76026d4f41d38a9b8769b72d7a5575ba0595d
Thales Janisch Santos <33602977+Thales3006@users.noreply.github.com>
2023-11-23 14:33:51 -0300

    pistas

--- bibs.h
+++ bibs.h
@@ -35,4 +35,6 @@ int tamanho_pista(pista* a);
 int tamanho_in_pista(pista* a);
 aviao* info_Pista(pista* a, int posP, int posA);
 aviao* info(aviao* a, int pos);
+
+void pistas(pista** a,int n);
 void decolar(pista** aeroporto, aviao** ceu, int pista);
